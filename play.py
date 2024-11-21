70780813f516edb833d3d053c23229a3819a7d8a
seamusprs <seamusprs>
2024-11-21 10:52:41 -0800

    update play

--- play.py
+++ play.py
@@ -1,4 +1,3 @@
-import stats.readdata as rd
 import stats.showstats as ss
 
 print("Welcome to Spot Your Spotify!")
@@ -8,16 +7,18 @@ inp = ""
 
 while inp != "exit":
     inp = input("Enter path to playlist csv without file extension: ").lower()
-    if inp == "": pl = rd.Playlist("playlist")
+    if inp == "exit": break
+    if inp == "": pl = ss.Playlist("playlist")
     else:
         try:
-            pl = rd.Playlist(inp)
+            pl = ss.Playlist(inp)
         except:
             print("Invalid file path! Please try again, or leave blank to use example playlist")
             continue
-
+    
     while inp != "exit":
         print("Please enter 'stats' for playlist information, 'quiz' for the quiz game, 'new' to change playlist, or 'exit' to exit.")
+        inp = input("Enter your choice: ").lower()
         if inp == "exit" or inp == "new": 
             break
         elif inp == "stats": 
